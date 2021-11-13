e0335e65095b3565f933bdb7b06787dd0d66ef30
NeogioCodeMaster <limav2a@gmail.com>
2021-11-12 21:54:39 -0500

    Carga inicial del Proyecto Zona Unica

---
+++ src/main/java/com/zonaunica/ZonaUnica/ZonaUnicaApplication.java
@@ -0,0 +1,13 @@
+package com.zonaunica.ZonaUnica;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZonaUnicaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZonaUnicaApplication.class, args);
+	}
+
+}
