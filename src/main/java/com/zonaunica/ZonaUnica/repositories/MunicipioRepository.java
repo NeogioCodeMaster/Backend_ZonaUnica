8646b2308feef6ad27cb37c065e76a0c903d8567
Mascenciog <67958395+Mascenciog@users.noreply.github.com>
2021-11-15 17:35:10 -0500

    Create MunicipioRepository.java

---
+++ src/main/java/com/zonaunica/ZonaUnica/repositories/MunicipioRepository.java
@@ -0,0 +1,9 @@
+package com.zonaunica.ZonaUnica.repositories;
+
+import com.zonaunica.ZonaUnica.models.MunicipioModel;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface MunicipioRepository extends MongoRepository <MunicipioModel, String>{
+    
+}
